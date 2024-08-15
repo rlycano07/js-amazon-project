1605815f5eee59689d3ba2b2fc004b843bfd76c3
rushan-interserv <baral.rushan@gmail.com>
2024-08-15 22:44:17 +1000

    feat: intialising cart file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart">
                 Add to Cart
             </button>
         </div>
@@ -54,3 +54,9 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('added product');
+    })
+});
