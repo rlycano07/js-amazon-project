bc4c6ef141517be6f132a76a6645e410690534a0
rushan-interserv <baral.rushan@gmail.com>
2024-10-09 20:07:04 +1100

    feat: save data and generate html for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSymmary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
