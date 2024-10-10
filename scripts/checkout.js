a01d86063b2609d51df63e714c228333fddac3b2
rushan-interserv <baral.rushan@gmail.com>
2024-10-10 18:12:27 +1100

    feat: create oop version of carts

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSymmary.js";
 import { numberOfItemsInCart } from "../data/cart.js";
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
