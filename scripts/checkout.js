0f162660a17601feb172f133a9c7605c469801da
rushan-interserv <baral.rushan@gmail.com>
2024-10-10 18:28:55 +1100

    feat: create class version of carts

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSymmary.js";
 import { numberOfItemsInCart } from "../data/cart.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
